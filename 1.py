ebd5b92982fbe949c304068e28079ca3eed6091d
Sergey Mironov <afgjz.06@gmail.com>
2022-09-01 17:06:24 +0300

    changed all

--- 1.py
+++
@@ -1,4 +0,0 @@
-import yadisk
-
-y = yadisk.YaDisk(token="y0_AgAAAAAf2yxeAADLWwAAAADM9GjtVAOq_w5dQquZBqgOH-9l1xuv9nM")
-y.upload("https://sun9-77.userapi.com/impg/Ai74rUZVqTGN_Vzaf637m9IE6vOpghhxGZyloQ/kxAfLUMxnnY.jpg?size=600x337&quality=96&sign=f12d89aac2c397350cb178ef3d378296&c_uniq_tag=ezVz7Hz__kp1MZimjNIXETas9OdPh7sY5uM-w6YdwW8&type=album", "/new_reposit")
--- kursach.py
+++ kursach.py
@@ -1,11 +1,15 @@
-from time import sleep
 import requests
 from pprint import pprint
+import json
+from progress.bar import IncrementalBar
+from time import sleep
+
+mylist = [1,2]
+
+bar = IncrementalBar('Countdown', max = len(mylist))
 
 
 class photos_vk:
-    list_sizes_url= []
-    all_on_json_list__= []
     url= 'https://api.vk.com/method/'
     def __init__(self):
         self.params= {
@@ -15,8 +19,8 @@ class photos_vk:
 
         return
 
-    
-    def get_photos_at_vk(self , id: str):
+
+    def get_photos_at_vk(self , id: str): #получение названий и размеров для json-чика и соединение
         photos_url= photos_vk.url + 'photos.get'
         params= {
             'album_id': 'profile',
@@ -24,7 +28,6 @@ class photos_vk:
             'owner_id': id
         }
         response= requests.get(photos_url, params={**self.params, **params})
-        #получение названий и размеров для json-чика и соединение
         r= response.json()
         lists =  r['response']['items']
 
@@ -42,16 +45,17 @@ class photos_vk:
             end_dict_size= size_list[-1]
             h= end_dict_size['height']
             w= end_dict_size['width']
-            dict_size_2['size']= [f'{h} * {w}', end_dict_size['url']]
+            dict_size_2['size']= f'{h} * {w}'
             list_sizes.append(dict_size_2)
 
-        
+        all_on_json_list__= []
         for k in zip(list_likes, list_sizes):
-            photos_vk.all_on_json_list__.append({**k[0], **k[1]})
+            all_on_json_list__.append({**k[0], **k[1]})
+        return all_on_json_list__ 
 
-        return photos_vk.all_on_json_list__ #ебануть размерчики сюда
 
-    def size_url(self, id: str):  #Получаю список для загрузки ядиск(формат лайк:юрлик), я не смог заковать lists в global, написал второй раз
+    def size_url(self, id: str):  #Получаю список для загрузки на диск(формат лайк:юрл), я не смог перевести lists в global, написал второй раз
+                                  #получение response
         photos_url= photos_vk.url + 'photos.get'
         params= {
             'album_id': 'profile',
@@ -59,7 +63,6 @@ class photos_vk:
             'owner_id': id
         }
         response= requests.get(photos_url, params={**self.params, **params})
-        #получение названий и размеров для json-чика и соединение
         r= response.json()
         lists =  r['response']['items']
 
@@ -75,13 +78,8 @@ class photos_vk:
             list_sizes.append(end_dict_size['url'])
 
         _zipeed_size= list(zip(list_likes, list_sizes))
-        
-
         return(_zipeed_size)
-            
 
-            
-    
 
 
 class Yadisk:
@@ -89,52 +87,37 @@ class Yadisk:
     def __init__(self):
         self.headers= {
             "Content-type": "application/json",
-            'Authorization': 'OAuth y0_AgAAAAAf2yxeAADLWwAAAADM9GjtVAOq_w5dQquZBqgOH-9l1xuv9nM' 
+            'Authorization': 'OAuth y0_AgAAAAAf2yxeAADLWwAAAADM9GjtVAOq_w5dQquZBqgOH-9l1xuv9nM',
+            'Accept': 'application/json'
             }
 
+
     def create_folder(self, folder_name,vk_id ,range_= 5):
-        headers_= {'Accept': 'application/json'}
-        response= requests.put(Yadisk.url, headers= {**self.headers,**headers_}, params= f'path= {folder_name}')
+        response= requests.put(Yadisk.url, headers=self.headers, params= f'path={folder_name}')
         list_in_def= photo.size_url(vk_id)
         renged_list= list_in_def [:range_]
-        for name,size in renged_list:   
-            pprint(name)
-            pprint(size)
-            response_up= requests.post(Yadisk.url+ '/upload', headers={**self.headers, **headers_},params={'url':size, 'path': f'{folder_name}/{name}' } )
-            return response_up.json()
 
+        with open('1.json', 'w', encoding='utf-8') as f:
+            list_for_json= photo.get_photos_at_vk(vk_id)
+            r_list_for_json= list_for_json[:range_]
+            json.dump(r_list_for_json, f)
+        bar.next()
+        sleep(1)
+        pprint('Запись в json прошла успешно')
 
+        for name,size in renged_list:
+            response_up= requests.post(Yadisk.url+ '/upload',
+             headers=self.headers,
+             params={'url':{size}, 'path':f'{folder_name}/{name}' } )
+        bar.next()
+        sleep(1)
+        pprint('Данные успешно отправлены в облако')
+        bar.finish()
+        
+        return
 
-   # def status(self):
-   #     response= requests.get('https://cloud-api.yandex.net/v1/disk/', headers= self.headers)
-    #    return response.json()
 
 
 photo= photos_vk()
 put_photos= Yadisk()
-pprint(put_photos.create_folder('new_reposit',vk_id= 411548195))
-#pprint(put_photos.status())
-#pprint(photo.size_url())
-
-
-
-
-
-
-
-
-#headers_= {'Accept': 'application/json'
- #           "Content-type": "application/json",
- #           'Authorization': 'OAuth y0_AgAAAAAf2yxeAADLWwAAAADM9GjtVAOq_w5dQquZBqgOH-9l1xuv9nM' 
-  #          }
-
-
-
-
-
-#yandex
- #       sizes_list= []
-     #   for dict_size in lists:
-      #      size_list= dict_size['sizes']
-       #     end_dict_size= size_list[-1]
-        #    sizes_list.append(end_dict_size['url'])
+put_photos.create_folder('new_reposit',vk_id= 411548195)
